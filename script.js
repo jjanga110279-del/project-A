7eb807f757f5a8a78cf1ec8adfef9cbaa6275ca0
jjanga110279-del <jjanga110279@gmail.com>
2026-02-05 09:28:21 +0900

    최초커밋

--- script.js
+++ script.js
@@ -3,33 +3,29 @@ const btn = document.querySelector(".popup button");
 btn.onclick = () => {
   document.querySelector(".popup").style.display = "none";
 };
-c;
-
+document.querySelector(".popup").style.display = "none";
 //메인 배너 가로스크롤
-document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".main-banner.swiper", {
-    loop: true,
-    speed: 800,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
+const swiper = new Swiper(".main-banner.swiper", {
+  loop: true,
+  speed: 800,
+  //autoplay: {
+  //  delay: 4000,
+  //  disableOnInteraction: false,
+  //},
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+  observer: true,
+  observeParents: true,
+  watchOverflow: true,
+  on: {
+    resize: function () {
+      this.update();
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 3,
-      },
-    },
-    observer: true,
-    observeParents: true,
-    watchOverflow: true,
-    on: {
-      resize: function () {
-        this.update();
-      },
-    },
-  });
+  },
 });
